0c011fc5ddf9705e0ee253760ab3e2e679f9f9d5
xiecie <47521766+xiecie@users.noreply.github.com>
2021-02-13 19:06:47 +0100

    nodejs + yarn + pm2

---
+++ nodejs.sh
@@ -0,0 +1,6 @@
+apt-get install -y curl 
+wget -qO- https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install -y yarn nodejs
+yarn global add pm2
