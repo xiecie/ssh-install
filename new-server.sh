85e42392f4cc203a7786b4e3dec098b5bd1f6f70
xiecie <47521766+xiecie@users.noreply.github.com>
2021-02-21 09:14:03 +0100

    Delete new-server.sh

--- new-server.sh
+++
@@ -1,9 +0,0 @@
-
-apt-get install -y curl 
-wget -qO- https://deb.nodesource.com/setup_14.x | -E bash -
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && sudo apt-get install -y yarn nodejs docker.io docker-compose cockpit
-yarn global add pm2
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
